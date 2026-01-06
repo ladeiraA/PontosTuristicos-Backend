a89ef17d0d52a856e302635b238c8b86304002a0
Ladeira <cesar-ladeira1@hotmail.com>
2026-01-06 09:10:58 -0300

    Ajusta entidade PontoTuristico para campos alinhados ao enunciado

---
+++ PontosTuristicos.Api/Models/PontoTuristico.cs
@@ -0,0 +1,27 @@
+﻿namespace PontosTuristicos.Api.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class PontoTuristico
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string? Descricao { get; set; } =string.Empty;
+
+        [Required]
+        public string Referencia { get; set; } = string.Empty;
+
+        [Required]
+        public string Cidade { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(2)]
+        public string Estado { get; set; } = string.Empty;
+
+        public DateTime DataInclusao { get; set; } = DateTime.Now;
+    }
+}
