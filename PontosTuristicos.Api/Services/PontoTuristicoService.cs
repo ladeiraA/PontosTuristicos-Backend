9c4ab0da6f5b6732603e246ff11f725344bc9878
Ladeira <cesar-ladeira1@hotmail.com>
2026-01-06 10:41:28 -0300

    Cria service com regra de negócio para PontoTuristico

---
+++ PontosTuristicos.Api/Services/PontoTuristicoService.cs
@@ -0,0 +1,31 @@
+﻿using PontosTuristicos.Api.Models;
+using PontosTuristicos.Api.Repositories;
+
+namespace PontosTuristicos.Api.Services;
+
+public class PontoTuristicoService
+{
+    private readonly IPontoTuristicoRepository _repository;
+
+    public PontoTuristicoService(IPontoTuristicoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<IEnumerable<PontoTuristico>> ListarAsync(
+        string? termo, int pagina, int tamanhoPagina)
+    {
+        return _repository.ListarAsync(termo, pagina, tamanhoPagina);
+    }
+
+    public Task<PontoTuristico?> ObterPorIdAsync(int id)
+    {
+        return _repository.ObterPorIdAsync(id);
+    }
+
+    public Task CriarAsync(PontoTuristico ponto)
+    {
+        ponto.DataInclusao = DateTime.Now;
+        return _repository.AdicionarAsync(ponto);
+    }
+}
